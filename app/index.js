3600b01ec7ab9056bb406a151aaaae72cc3cc6b8
Jelte Homminga <jeltehomminga@gmail.com>
2019-08-04 21:28:55 +0200

    first component

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  render() {
+    return <div>Hello Stella!</div>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
